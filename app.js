3df7045438ffaf93f9ba126223758d9ef2a2df84
adam <delsarte.adam@gmail.com>
2024-07-24 12:21:22 +0200

    sql try

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "restaurant.db",
+});
+
+// async IIFE
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection to the database successful!");
+  } catch (error) {
+    console.error("Error connecting to the database: ", error);
+  }
+})();
 
 const app = express();
 
