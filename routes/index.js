b366d4f25a3fbbbc85393542db13812bf1eec8f6
adam <delsarte.adam@gmail.com>
2024-07-24 12:00:53 +0200

    site on

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,24 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
+router.get("/menu", (req, res) => {
+  res.render("menu");
+});
+
+router.get("/restaurant", (req, res) => {
+  res.render("restaurant");
+});
+
+router.get("/pictures", (req, res) => {
+  res.render("pictures");
+});
+
+router.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+router.get("/admin", (req, res) => {
+  res.render("backoffice");
+});
+
 module.exports = router;
